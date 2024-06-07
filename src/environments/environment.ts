c82a6f6356c68c40eedb9fc503ea4b8d20251ca7
Jesus Duque <jesusduque@fii-na.com.mx>
2024-06-07 15:27:58 -0600

    change in variable environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
   apiUrl: 'https://10.19.17.84:9119',
-  serverUrl: 'https://10.19.17.84:2423',
+  serverUrl: 'https://10.19.17.34:2423',
   webSocketUrl: '10.19.17.84:9229',
   version: 'UAT-2024060701'
 };
