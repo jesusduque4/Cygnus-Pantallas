b73fce864de273a6122d398f7c2e30eacc94be48
Jesus Duque <jesusduque@fii-na.com.mx>
2024-06-11 11:27:32 -0600

    change colors in cardsections

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://10.19.17.34:9119',
+  apiUrl: 'https://10.19.17.34:1991',
   serverUrl: 'https://10.19.17.34:2423',
   webSocketUrl: '10.19.17.34:9229',
   version: 'UAT-2024060701'
