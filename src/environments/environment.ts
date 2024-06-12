a99275e54ed2ee9250b83b788b12d265c069d1e4
Jesus Duque <jesusduque@fii-na.com.mx>
2024-06-12 16:01:21 -0600

    change port api UAT

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://10.19.17.84:9119',
+  apiUrl: 'https://10.19.17.84:1991',
   serverUrl: 'https://10.19.17.84:2324',
   webSocketUrl: '10.19.17.84:9229',
   version: 'UAT-2024060701'
